eca71c56c9553c02673c3fa03193c37bc680770b
amrit22 <amritrai22oct@gmail.com>
2025-05-23 12:41:11 +0530

    change the file structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Import path module
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // This maps "@" to "src"
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      // This proxies all API requests to the backend running at port 5001
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/uploads': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 });
